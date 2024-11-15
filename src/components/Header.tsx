789367c7ce28fe276c81f4a0ac514a1e809fabe2
guillaume dewailly <dewailly.freelance@gmail.com>
2024-11-15 14:11:17 +0100

    resolve url img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ export function Header() {
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                 >
-                  <img src="/sixty.png" />
+                  <img src="/src/sixty.png" />
                 </motion.h1>
               </Link>
             </div>
