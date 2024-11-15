80a309cf480eaea8d41ecc083087a37bcdab5190
guillaume dewailly <dewailly.freelance@gmail.com>
2024-11-15 14:15:24 +0100

    add img 3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ export function Header() {
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                 >
-<img className='max-w-5' src={sixtyImage} alt="Description de l'image" />
+<img className='max-w-10' src={sixtyImage} alt="Description de l'image" />
 </motion.h1>
               </Link>
             </div>
